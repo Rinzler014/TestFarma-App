78c698e3bbde4dfdf89b90b4779b867dffda61eb
Ricardo Adolfo González Terán <ricardo_glez_14@icloud.com>
2022-09-27 16:25:08 -0500

    Initialized NavHost Control

--- app/src/main/java/com/example/testfarma/MainActivity.kt
+++ app/src/main/java/com/example/testfarma/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.testfarma
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.testfarma.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Setting up the Host of the Fragments
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || navController.navigateUp()
+    }
+
 }
