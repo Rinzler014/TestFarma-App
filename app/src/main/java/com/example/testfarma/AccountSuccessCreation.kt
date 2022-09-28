78d00a48480f833f1f55b830fb730d054b088810
Ricardo Adolfo González Terán <ricardo_glez_14@icloud.com>
2022-09-28 14:55:17 -0500

    Merged Menu and Creation Account Form

---
+++ app/src/main/java/com/example/testfarma/AccountSuccessCreation.kt
@@ -0,0 +1,54 @@
+package com.example.testfarma
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.testfarma.databinding.FragmentAccountSuccessCreationBinding
+
+
+class AccountSuccessCreation : Fragment() {
+
+    private var _binding : FragmentAccountSuccessCreationBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (activity as AppCompatActivity).supportActionBar?.show()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAccountSuccessCreationBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val button_to_login = view.findViewById<Button>(R.id.button_to_login)
+
+        button_to_login.setOnClickListener {
+
+            val action = AccountSuccessCreationDirections.actionAccountSuccessCreation2ToNavigation2()
+            view.findNavController().navigate(action)
+
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
---
+++ app/src/main/java/com/example/testfarma/ui/slideshow/SlideshowFragment.kt
@@ -0,0 +1,35 @@
+package com.example.testfarma.ui.slideshow
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.testfarma.databinding.FragmentSlideshowBinding
+
+class SlideshowFragment : Fragment() {
+
+    private var _binding: FragmentSlideshowBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
