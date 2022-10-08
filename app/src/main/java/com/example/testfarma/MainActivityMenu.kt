ca532c77ab61fa62aafdc5b56482090cda0bc95b
Ricardo Adolfo González Terán <ricardo_glez_14@icloud.com>
2022-10-07 22:37:06 -0500

    Added Logout button and recover password
    Deleted mail button

--- app/src/main/java/com/example/testfarma/MainActivityMenu.kt
+++ app/src/main/java/com/example/testfarma/MainActivityMenu.kt
@@ -1,6 +1,7 @@
 package com.example.testfarma
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -11,6 +12,8 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testfarma.databinding.ActivityMainMenuBinding
+import com.google.android.material.internal.NavigationMenuItemView
+import com.google.android.material.navigation.NavigationBarItemView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -35,15 +38,18 @@ class MainActivityMenu : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.configuracion_perfil, R.id.contacto
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.configuracion_perfil, R.id.contacto, R.id.logOut
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
 }
--- app/src/main/java/com/example/testfarma/ResetPassword.kt
+++ app/src/main/java/com/example/testfarma/ResetPassword.kt
@@ -2,11 +2,13 @@ package com.example.testfarma
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import com.example.testfarma.databinding.FragmentResetPasswordBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -41,16 +43,39 @@ class ResetPassword : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val email = view.findViewById<EditText>(R.id.field_reset_email_edit)
+
         val resetButton = view.findViewById<Button>(R.id.reset_button)
 
         resetButton.setOnClickListener {
 
-            val intent = Intent(context, PopUpWindow::class.java)
-            intent.putExtra("popuptitle", "Recuperacion de Contraseña")
-            intent.putExtra("popuptext", getString(R.string.pop_up_label_message))
-            intent.putExtra("popupbtn", getString(R.string.continue_label))
-            intent.putExtra("darkstatusbar", false)
-            startActivity(intent)
+            auth.sendPasswordResetEmail(email.text.toString()).addOnCompleteListener {
+
+                if(it.isSuccessful) {
+
+                    Log.d("MAIN", email.text.toString())
+                    val intent = Intent(context, PopUpWindow::class.java)
+                    intent.putExtra("popuptitle", "Recuperacion de Contraseña")
+                    intent.putExtra("popuptext", getString(R.string.pop_up_label_message))
+                    intent.putExtra("popupbtn", getString(R.string.continue_label))
+                    intent.putExtra("darkstatusbar", false)
+                    startActivity(intent)
+
+                } else {
+
+                    Log.w("MAIN", "Error sending email", it.exception)
+                    val intent = Intent(context, PopUpWindow::class.java)
+                    intent.putExtra("popuptitle", "Recuperacion de Contraseña")
+                    intent.putExtra("popuptext", "Error al recuperar contraseña, vuelve a intentarlo")
+                    intent.putExtra("popupbtn", getString(R.string.continue_label))
+                    intent.putExtra("darkstatusbar", false)
+                    startActivity(intent)
+
+                }
+
+            }
+
+
 
         }
 
