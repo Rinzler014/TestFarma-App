5a8abdc59a0a94406f3bb7733442923ce213ee29
PabloGP88 <pablogpp88@gmail.com>
2022-10-10 08:35:44 -0500

    cambios en color boton y pagina de estudios agregada

---
+++ app/src/main/java/com/example/testfarma/StudiesPage.kt
@@ -0,0 +1,11 @@
+package com.example.testfarma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StudiesPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_studies_page)
+    }
+}
