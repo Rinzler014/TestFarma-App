5fbf4dbc5127a33d85c0a73ff8feb1d76fe3bf6f
PabloGP88 <pablogpp88@gmail.com>
2022-10-12 23:43:50 -0500

    estudios

--- app/src/main/java/com/example/testfarma/StudiesPage.kt
+++ app/src/main/java/com/example/testfarma/StudiesPage.kt
@@ -1,11 +1,21 @@
 package com.example.testfarma
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class StudiesPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_studies_page)
+
+        val button = findViewById<Button>(R.id.estudios1);
+
+        button.setOnClickListener {
+            val intent = Intent(this, pdfReader::class.java);
+            startActivity(intent) ;
+        }
+
     }
 }
